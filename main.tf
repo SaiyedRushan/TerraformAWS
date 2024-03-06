a35bc119804c0efc064998bb78ed695738ccba66
SaiyedRushan <rushan52@gmail.com>
2024-03-05 22:49:53 -0500

    create a network interface

--- main.tf
+++ main.tf
@@ -99,3 +99,9 @@ resource "aws_security_group" "allow_web" {
     Name = "allow_web"
   }
 }
+
+resource "aws_network_interface" "web-server-nic" {
+  subnet_id       = aws_subnet.subnet-1.id
+  private_ips     = ["10.0.1.50"]
+  security_groups = [aws_security_group.allow_web.id]
+}
