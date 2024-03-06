f9605ffc5efab9bba4b03de97a0f99d9215bb62a
SaiyedRushan <rushan52@gmail.com>
2024-03-05 22:48:02 -0500

    create subnet

--- main.tf
+++ main.tf
@@ -34,18 +34,24 @@ resource "aws_internet_gateway" "gw" {
 
 resource "aws_route_table" "prod-route-table" {
   vpc_id = aws_vpc.prod-vpc.id
-
   route {
     cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.gw.id
   }
-
   route {
     ipv6_cidr_block = "::/0"
     gateway_id      = aws_internet_gateway.gw.id
   }
-
   tags = {
     Name = "Prod"
+  }
+}
+
+resource "aws_subnet" "subnet-1" {
+  vpc_id            = aws_vpc.prod-vpc.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "us-east-1a"
+  tags = {
+    Name = "prod-subnet"
   }
 }
