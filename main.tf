4799a48f11af4cce87838259ebc19e9afaca802d
SaiyedRushan <rushan52@gmail.com>
2024-03-05 22:51:27 -0500

    assign an elastic ip to the network interface

--- main.tf
+++ main.tf
@@ -105,3 +105,14 @@ resource "aws_network_interface" "web-server-nic" {
   private_ips     = ["10.0.1.50"]
   security_groups = [aws_security_group.allow_web.id]
 }
+
+resource "aws_eip" "one" {
+  domain                    = "vpc"
+  network_interface         = aws_network_interface.web-server-nic.id
+  associate_with_private_ip = "10.0.1.50"
+  depends_on                = [aws_internet_gateway.gw]
+}
+
+output "server_public_ip" {
+  value = aws_eip.one.public_ip
+}
