5fadcf21347f7e4215e997fdc3aaca4033e853bb
SaiyedRushan <rushan52@gmail.com>
2024-03-06 16:19:55 -0500

    specify vpc region

--- architecture.py
+++ architecture.py
@@ -12,16 +12,17 @@ from diagrams.aws.network import (
 from diagrams.aws.management import SystemsManager
 
 with Diagram("Terraform AWS Architecture", show=False, direction="LR"):
-    vpc = VPC("prod-vpc")
 
-    with Cluster("Availability Zone us-east-1a"):
-        subnet = PublicSubnet("subnet-1")
-        igw = InternetGateway("gw")
-        rt = RouteTable("prod-route-table")
-        sg = ELB("allow_web")  # Using ELB as a stand-in for a security group
+    with Cluster("Region us-east-1"):
+        vpc = VPC("prod-vpc")
+        with Cluster("Availability Zone us-east-1a"):
+            subnet = PublicSubnet("subnet-1")
+            igw = InternetGateway("GW")
+            rt = RouteTable("prod-route-table")
+            sg = ELB("allow_web")  # Using ELB as a stand-in for a security group
 
-        instance = EC2("web-server-instance")
-        eip = ALB("eip")  # Using ALB as a placeholder for Elastic IP
+            instance = EC2("web-server-instance")
+            eip = ALB("EIP")  # Using ALB as a placeholder for Elastic IP
 
     vpc >> Edge(label="association") >> subnet
     igw >> Edge(label="0.0.0.0/0") >> rt
