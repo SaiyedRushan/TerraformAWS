902abf2a4e542bdfc94cb64e6fc5f52ee8b91442
SaiyedRushan <rushan52@gmail.com>
2024-03-05 22:57:42 -0500

    create ubuntu server and bootstrap with apache

--- main.tf
+++ main.tf
@@ -116,3 +116,34 @@ resource "aws_eip" "one" {
 output "server_public_ip" {
   value = aws_eip.one.public_ip
 }
+
+resource "aws_instance" "web-server-instance" {
+  ami               = "ami-07d9b9ddc6cd8dd30"
+  instance_type     = "t2.micro"
+  availability_zone = "us-east-1a"
+  key_name          = "terraform-project"
+
+  network_interface {
+    device_index         = 0
+    network_interface_id = aws_network_interface.web-server-nic.id
+  }
+
+  user_data = <<-EOF
+                #!/bin/bash
+                sudo apt update -y
+                sudo apt install apache2 -y
+                sudo systemctl start apache2
+                sudo bash -c 'echo your very first web server > /var/www/html/index.html'
+                EOF
+  tags = {
+    Name = "web-server"
+  }
+}
+
+output "server_private_ip" {
+  value = aws_instance.web-server-instance.private_ip
+}
+
+output "server_id" {
+  value = aws_instance.web-server-instance.id
+}
